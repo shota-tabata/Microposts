77aa04462af49e0a322970e02129325fd8bd38ba
shota.tabata <yotubato4289@gmail.com>
2021-05-05 13:28:16 +0000

    follow, unfollow

--- database/migrations/2021_05_04_211933_create_user_follow_table.php
+++ database/migrations/2021_05_04_211933_create_user_follow_table.php
@@ -15,15 +15,15 @@ class CreateUserFollowTable extends Migration
     {
         Schema::create('user_follow', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->usignedBigInteger('user_id');
-            $table->usignedBigInteger('follow_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('follow_id');
             $table->timestamps();
-            
-            //外部キー制約
+
+            // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('follow_id')->references('id')->on('users')->onDelete('cascade');
-            
-            //user_idとfollow_idの組み合わせの重複を許さない
+
+            // user_idとfollow_idの組み合わせの重複を許さない
             $table->unique(['user_id', 'follow_id']);
         });
     }
